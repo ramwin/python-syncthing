00d22bdbffc7aa409025620cb4e575ac22aecb03
Xiang Wang <ramwin@qq.com>
2022-06-17 21:33:28 +0800

    Bump deps

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     url = 'https://github.com/blakev/python-syncthing',
     license = 'The MIT License',
     install_requires = [
-        'python-dateutil==2.8.1',
-        'requests==2.24.0'
+        'python-dateutil>=2.8.1,<=2.8.2',
+        'requests>=2.24.0,<=2.28.0'
     ],
     extras_require = {
         'dev': [
