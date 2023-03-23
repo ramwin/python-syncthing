39591ed92b4ba57c363c864d1ea0cdc686036de4
Xiang Wang <ramwin@qq.com>
2023-03-23 21:59:15 +0800

    fix: update description

--- setup.py
+++ setup.py
@@ -1,36 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""Python bindings to the Syncthing REST interface, targeting v0.14.44"""
+
+
+from pathlib import Path
+
 from setuptools import setup
 
+
+with open(Path(__file__).parent.joinpath('README.md'),
+          encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name = 'syncthing2',
-    version = '2.4.4',
-    author = 'Blake VandeMerwe',
-    author_email = 'blakev@null.net',
-    description = 'Python bindings to the Syncthing REST interface, targeting v0.14.44',
-    url = 'https://github.com/blakev/python-syncthing',
-    license = 'The MIT License',
-    install_requires = [
+    name='syncthing2',
+    version='2.4.5',
+    author='Blake VandeMerwe',
+    author_email='blakev@null.net',
+    description='Python bindings to the Syncthing REST interface, targeting v0.14.44',
+    long_description=long_description,
+    url='https://github.com/blakev/python-syncthing',
+    license='The MIT License',
+    install_requires=[
         'python-dateutil>=2.8.1,<=2.8.2',
         'requests>=2.24.0,<3'
     ],
-    extras_require = {
+    extras_require={
         'dev': [
             'sphinx',
             'sphinxcontrib-napoleon',
             'sphinx_rtd_theme'
         ]
     },
-    packages = [
+    packages=[
         'syncthing'
     ],
-    package_dir = {
+    package_dir={
         'syncthing': 'syncthing'
     },
-    include_package_data = True,
-    zip_safe = True,
-    keywords = 'syncthing,sync,rest,backup,api',
+    include_package_data=True,
+    zip_safe=True,
+    keywords='syncthing,sync,rest,backup,api',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -44,4 +55,5 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Topic :: System :: Archiving :: Mirroring'
     ],
+    data_files=[('README.md', ['README.md'])],
 )
