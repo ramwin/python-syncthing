0a3dc3f4b557989babdb63cf223a31863553fcaf
Xiang Wang <ramwin@qq.com>
2024-09-11 15:24:26 +0800

    fix: update requests version to < 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'syncthing',
-    version = '2.4.3',
+    version = '2.4.6',
     author = 'Blake VandeMerwe',
     author_email = 'blakev@null.net',
     description = 'Python bindings to the Syncthing REST interface, targeting v0.14.44',
@@ -13,7 +13,7 @@ setup(
     license = 'The MIT License',
     install_requires = [
         'python-dateutil>=2.8.1,<=2.8.2',
-        'requests>=2.24.0,<=2.28.0'
+        'requests>=2.24.0,<3',
     ],
     extras_require = {
         'dev': [
